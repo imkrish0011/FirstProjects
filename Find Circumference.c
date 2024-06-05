081a577a18bdf9e3876f6c16cc0b24415467e1fa
Krish Sharma <ks9875277@gmail.com>
2024-06-05 15:11:27 +0530

    Add files via upload

---
+++ Find Circumference.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main(){
+
+   float radius;
+   printf("Radius :");
+   scanf("%f",&radius);
+
+   float Circumference = 2*3.14*radius;
+   printf("Circumference : %f",Circumference);
+
+
+    return 0; 
+}
---
+++ Find area of square.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main(){
+
+    int a;
+    printf("Write side of square:");
+    scanf("%d",&a);
+    
+    int area = a*a;
+
+    printf("area of square : %d" , area);
+
+    return 0; 
+}
